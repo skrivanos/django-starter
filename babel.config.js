eb24d799269cbef0c4e9cf26e41830d956e1ba41
Alexander Börjesson Skrivanos <alex@skrivanos.se>
2019-01-31 22:36:19 +0100

    Fix babel config

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'presets': [
+    '@babel/preset-env'
+  ],
+  'ignore': [
+    'node_modules/',
+    'dist/'
+  ],
+  'plugins': [
+    '@babel/plugin-proposal-object-rest-spread'
+  ]
+};
