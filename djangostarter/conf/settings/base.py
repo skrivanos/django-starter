039e53237e675d319fa3a4d87745feba8411e348
Alexander Skrivanos <alex@skrivanos.se>
2022-04-30 11:53:10 +0100

    Upgrade packages to the latest versions

--- djangostarter/conf/settings/base.py
+++ djangostarter/conf/settings/base.py
@@ -1,5 +1,5 @@
 """
-https://docs.djangoproject.com/en/2.1/ref/settings/
+https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
@@ -17,7 +17,7 @@ if os.path.exists(dotenv):
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'REPLACE THIS SECRET KEY!'
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'djangostarter.conf.wsgi.application'
 
 
 # Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/2.1/topics/i18n/
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -118,12 +118,19 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
 STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Auth
