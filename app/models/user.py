eab29034a22963466fe7ade641dabcb214b72593
Alexander Skrivanos <alex@skrivanos.se>
2022-06-18 14:01:21 +0200

    Add @final to User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,11 @@
+from typing import final
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+@final
 class User(AbstractUser):
     """
     Custom user model.
