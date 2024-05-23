94d28cb164f685783c8c896e88cb5fa66bd55b75
Alexander Skrivanos <alex@skrivanos.se>
2024-05-23 20:35:58 +0200

    Upgrade all deps

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@
 URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 
 Examples:
     Function views
@@ -15,6 +15,7 @@ Examples:
         1. Import the include() function: from django.urls import include, path
         2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
